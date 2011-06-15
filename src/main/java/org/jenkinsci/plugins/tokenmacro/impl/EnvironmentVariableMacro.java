8aa9a232e5e6e68407db0609403ccd5c49880106
Rob Petti <rpetti@autonomy.com>
2011-06-15 16:26:23 -0600

    adding environment variable macro

---
+++ src/main/java/org/jenkinsci/plugins/tokenmacro/impl/EnvironmentVariableMacro.java
@@ -0,0 +1,33 @@
+package org.jenkinsci.plugins.tokenmacro.impl;
+
+import hudson.model.AbstractBuild;
+import hudson.model.TaskListener;
+import java.io.IOException;
+import java.util.Map;
+import org.jenkinsci.plugins.tokenmacro.DataBoundTokenMacro;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+
+/**
+ * Token that expands variables from the build environment.
+ */
+public class EnvironmentVariableMacro extends DataBoundTokenMacro {
+
+    @Parameter(required=true)
+    public String var = "";
+
+    @Override
+    public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName)
+            throws MacroEvaluationException, IOException, InterruptedException {
+        Map<String, String> env = context.getEnvironment(listener);
+        if(env.containsKey(var)){
+            return env.get(var);
+        }
+        return "";
+    }
+
+    @Override
+    public boolean acceptsMacroName(String macroName) {
+        return macroName.equals("ENV");
+    }
+
+}
