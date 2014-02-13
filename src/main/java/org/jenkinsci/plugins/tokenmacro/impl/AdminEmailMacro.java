f45426adc1bc1892ccea8892616456f845aa33a4
Alex Earl <slide.o.mix@gmail.com>
2014-02-13 05:05:32 -0700

    Add ADMIN_EMAIL token

---
+++ src/main/java/org/jenkinsci/plugins/tokenmacro/impl/AdminEmailMacro.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jenkinsci.plugins.tokenmacro.impl;
+
+import com.google.common.collect.ListMultimap;
+import hudson.Extension;
+import hudson.model.AbstractBuild;
+import hudson.model.TaskListener;
+import java.io.IOException;
+import java.util.Map;
+import jenkins.model.JenkinsLocationConfiguration;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+
+/**
+ *
+ * @author acearl
+ */
+@Extension
+public class AdminEmailMacro extends TokenMacro {
+    
+    public static final String MACRO_NAME = "ADMIN_EMAIL";
+
+    @Override
+    public boolean acceptsMacroName(String macroName) {
+        return MACRO_NAME.equals(macroName);
+    }
+
+    @Override
+    public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+        return JenkinsLocationConfiguration.get().getAdminAddress();
+    }
+    
+}
---
+++ src/test/java/org/jenkinsci/plugins/tokenmacro/impl/AdminEmailMacroTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jenkinsci.plugins.tokenmacro.impl;
+
+import hudson.model.FreeStyleBuild;
+import hudson.model.FreeStyleProject;
+import hudson.util.StreamTaskListener;
+import jenkins.model.JenkinsLocationConfiguration;
+import static junit.framework.TestCase.assertEquals;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+/**
+ *
+ * @author acearl
+ */
+public class AdminEmailMacroTest {
+    
+    @Rule
+    public final JenkinsRule j = new JenkinsRule();
+
+    @Test
+    public void testAdminAddressMacro() throws Exception {
+        JenkinsLocationConfiguration.get().setAdminAddress("mickey@disney.com");
+        
+        FreeStyleProject p = j.createFreeStyleProject("foo");
+        FreeStyleBuild b = p.scheduleBuild2(0).get();
+        
+        assertEquals("mickey@disney.com",TokenMacro.expand(b, StreamTaskListener.fromStdout(),"${ADMIN_EMAIL}"));
+    }
+    
+}
