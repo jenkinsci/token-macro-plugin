65fabc0bb02fa64fc17fd5e1b12d39f3a9fa1eb5
Alex Earl <slide.o.mix@gmail.com>
2017-02-01 13:59:44 -0700

    Fix JENKINS-38871

--- src/main/java/org/jenkinsci/plugins/tokenmacro/DataBoundTokenMacro.java
+++ src/main/java/org/jenkinsci/plugins/tokenmacro/DataBoundTokenMacro.java
@@ -88,7 +88,7 @@ public abstract class DataBoundTokenMacro extends TokenMacro {
     /**
      * Builds up the {@link #setters} map that encapsulates where/how to set the value.
      */
-    private void buildMap() {
+    private synchronized void buildMap() {
         if (setters!=null)  return;
 
         setters = new ConcurrentHashMap<>();
