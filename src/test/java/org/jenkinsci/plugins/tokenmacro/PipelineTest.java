b222d73d0a3e14b1707c9b4d1a4a40662c0efbf1
Alex Earl <slide.o.mix@gmail.com>
2021-11-02 15:04:32 -0700

    fix: Make pipeline environment variables available [JENKINS-63551] (#87)
    * fix: Make pipeline environment variables available [JENKINS-63551]
    
    Also, some minor cleanups

--- src/test/java/org/jenkinsci/plugins/tokenmacro/PipelineTest.java
+++ src/test/java/org/jenkinsci/plugins/tokenmacro/PipelineTest.java
@@ -34,7 +34,27 @@ public class PipelineTest {
 
     @Before
     public void setup() throws Exception {
-	j.createOnlineSlave(Label.get("agents"));
+	    j.createOnlineSlave(Label.get("agents"));
+    }
+
+    @Test
+    public void testEnvironmentVariables() throws Exception {
+        WorkflowJob job = j.jenkins.createProject(WorkflowJob.class, "foo");
+        job.setDefinition(new CpsFlowDefinition("pipeline {\n" +
+                "  agent any\n" +
+                "  environment {\n" +
+                "    VERSION = \"1.0.0\"\n" +
+                "  }\n\n" +
+                "  stages {\n" +
+                "    stage('Cool') {\n" +
+                "      steps {\n" +
+                "        echo \"VERSION=\" + tm('${ENV, var=\"VERSION\"}')\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}", true));
+        Run<?,?> run = j.assertBuildStatusSuccess(job.scheduleBuild2(0));
+        j.assertLogContains("VERSION=1.0.0", run);
     }
 
     @Test
