ff7645a56ff0d42c6574a70c428edc2ec87b6fea
Alex Earl <slide.o.mix@gmail.com>
2022-03-24 19:23:43 -0700

    fix: JENKINS-67916 (#111)
    * Fix JENKINS-67916
    
    Add check for valid identifier
    
    * Fix spotbug issue with unused local

--- src/main/java/org/jenkinsci/plugins/tokenmacro/Parser.java
+++ src/main/java/org/jenkinsci/plugins/tokenmacro/Parser.java
@@ -109,7 +109,7 @@ public class Parser {
             parseEscapedToken(c);
         } else if(c.current() == '{') {
             parseDelimitedToken(c);
-        } else if(Character.isLetter(c.current())) {
+        } else if(Character.isLetter(c.current()) || c.current() == '_') {
             parseNonDelimitedToken(c);
         } else {
             output.append(last);
@@ -143,6 +143,14 @@ public class Parser {
         if(c.current() == '#') {
             addTransform(new ContentLengthTransform());
             c.next();
+        }
+
+        // check for valid identifier start
+        if(!Character.isLetter(c.current()) && c.current() != '_') {
+            output.append("${");
+            output.append(c.current());
+            c.next();
+            return;
         }
 
         String token = parseIdentifier(c);
