d7cc1f2a52c805672f1d09c9e3cb3a3d9c35a757
Alex Earl <slide.o.mix@gmail.com>
2022-11-17 05:43:46 -0800

    feat: Add BUILD_USER macro which returns the user who caused the build (#145)

---
+++ src/main/java/org/jenkinsci/plugins/tokenmacro/impl/BuildUserMacro.java
@@ -0,0 +1,45 @@
+package org.jenkinsci.plugins.tokenmacro.impl;
+
+import com.google.common.collect.ListMultimap;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.model.AbstractBuild;
+import hudson.model.Cause;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
+import org.jenkinsci.plugins.tokenmacro.TokenMacro;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Extension
+public class BuildUserMacro extends TokenMacro {
+    public static final String MACRO_NAME = "BUILD_USER";
+
+    @Override
+    public boolean acceptsMacroName(String macroName) {
+        return macroName.equals(MACRO_NAME);
+    }
+
+    @Override
+    public List<String> getAcceptedMacroNames() {
+        return Collections.singletonList(MACRO_NAME);
+    }
+
+    @Override
+    public String evaluate(AbstractBuild<?, ?> context, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+        return evaluate(context,null,listener,macroName,arguments,argumentMultimap);
+    }
+
+    @Override
+    public String evaluate(Run<?, ?> run, FilePath workspace, TaskListener listener, String macroName, Map<String, String> arguments, ListMultimap<String, String> argumentMultimap) throws MacroEvaluationException, IOException, InterruptedException {
+        Cause.UserIdCause userIdCause = run.getCause(Cause.UserIdCause.class);
+        if(userIdCause != null) {
+            return userIdCause.getUserId();
+        }
+        return Messages.TokenMacro_Unknown();
+    }
+}
---
+++ src/main/resources/org/jenkinsci/plugins/tokenmacro/impl/BuildUserMacro/help.groovy
@@ -0,0 +1,2 @@
+dt("\${BUILD_USER}")
+dd(_("Displays the user who caused the current build."))
